4be066f85d25e50ee2c534562d56cccdcb0e4aa7
Todd Lunter <tlunter@gmail.com>
2012-04-25 01:46:29 -0400

    Add zsh files and git files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,36 @@
+
+export EDITOR="vim"
+alias reload!=" source ~/.zshrc"
+alias aedit=" $EDITOR $ZSH/zsh/aliases.zsh; reload!"
+alias fedit=" $EDITOR $ZSH/zsh/functions.zsh; reload!"
+alias pedit=" $EDITOR $ZSH/zsh/prompt.zsh; reload!"
+alias oedit=" $EDITOR $ZSH/zsh/options.zsh; reload!"
+alias vedit=" $EDITOR $ZSH/vim/vimrc.symlink"
+alias gedit=" $EDITOR $ZSH/git/gitconfig.symlink"
+
+#alias man="unset PAGER; man"
+alias grep='grep --color=auto'
+
+##### standard aliases (start with a space to be ignored in history)
+# default ls is untouched, except coloring
+alias ls=' ls --color=auto'
+alias myls=' ls -Alph --color=always'
+alias l=" myls"
+alias ll=' myls -l'
+alias la=' myls -A'
+alias v=" clear; ll -g"    # standard directory view
+alias vs=" v **/*(.)"         # show all files in all subdirs plain in a list
+
+alias p=' ps aux | grep'
+alias g='git'
+alias d=' dirs -v'
+alias ka="killall"
+
+#alias .='xdg-open .'
+alias cd=' cd'
+alias ..=' cd ..; ls'
+alias ...=' cd ..; cd ..; ls'
+alias ....=' cd ..; cd ..; cd ..; ls'
+alias cd..='..'
+alias cd...='...'
+alias cd....='....'
---
+++ zsh/prompt.zsh
@@ -0,0 +1,78 @@
+# enable hook method
+autoload add-zsh-hook
+
+autoload colors && colors
+
+git_prompt_info () {
+ ref=$(/usr/bin/git symbolic-ref HEAD 2>/dev/null) || return
+# echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
+ echo "${ref#refs/heads/}"
+}
+
+unpushed () {
+  /usr/bin/git cherry -v @{upstream} 2>/dev/null
+}
+
+need_push () {
+  if [[ $(unpushed) == "" ]]
+  then
+    echo " "
+  else
+    echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
+  fi
+}
+
+
+# my prompt theme
+function promptSetup () {
+    setopt prompt_subst
+    local TERMWIDTH
+    (( TERMWIDTH = ${COLUMNS} - 1 ))
+
+    NOCOLOR="%{$terminfo[sgr0]%}"
+    PS1=''; RPS1='%d'
+    PS2="↷ %_>"; RPS2=''
+    PS3="↷ ?#"; RPS3=''
+    PS4="↷ +i>"; RPS3=''
+
+    # prepare vcs info
+    VCS_LINE=''
+    VCS_LINE+=$NOCOLOR
+    st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+    if [[ $st == "" ]]; then
+        echo ""
+    else
+        if [[ $st == "nothing to commit (working directory clean)" ]]; then
+            VCS_LINE+="➜ ± on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+        else
+            VCS_LINE+="➜ ± on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+        fi
+    fi
+
+    # rootshell gets another prompt sign
+    PR_SIGN=$NOCOLOR
+    PR_SIGN+="%F{160}%B"
+    PR_SIGN+=%(#."☠".'∴')
+    PR_SIGN+="%F{white}%b"
+
+    # Finally, the prompt.
+    if [[ ${#VCS_LINE} > 1 ]]; then
+        # http://unix.stackexchange.com/questions/1022/is-it-possible-to-display-stuff-below-the-prompt-at-a-prompt
+        terminfo_down_sc=$terminfo[cud1]$terminfo[cuu1]$terminfo[sc]$terminfo[cud1]
+
+    	PS1+="%{$terminfo_down_sc$VCS_LINE$terminfo[rc]%}" # the second line
+    fi
+
+    PS1+=$PR_SIGN                 # the user sign
+    PS1+=" "                      # an additional space
+}
+add-zsh-hook precmd promptSetup
+
+# remove the line after the prompt on execution
+# http://unix.stackexchange.com/questions/1022/is-it-possible-to-display-stuff-below-the-prompt-at-a-prompt
+function eraseSecondLine () {
+    print -rn -- $terminfo[el];
+    #echo; # this would keep the second line
+}
+add-zsh-hook preexec eraseSecondLine
+
