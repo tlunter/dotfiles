4b93956af116d80e9243456c62a27ad14f0c1ed0
Todd Lunter <tlunter@gmail.com>
2013-03-12 19:20:51 -0400

    Update the alias to a function so that you can supply a path now

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,2 +1,9 @@
 # Remove python compiled byte-code
-alias pyclean='find . -type f -name "*.py[co]" -exec rm -f \{\} \;'
+function pyclean {
+    if [[ -n $1 ]]
+    then
+        find $1 -type f -name "*.py[co]" -exec rm -f {} \;
+    else
+        find . -type f -name "*.py[co]" -exec rm -f {} \;
+    fi
+}
