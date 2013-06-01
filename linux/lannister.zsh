9b9eb74d13c443a4ea2a3a940c0bbcc59d01dab3
Todd Lunter <tlunter@gmail.com>
2013-06-01 14:19:57 -0400

    Lannister is being rebuilt from scratch

--- linux/lannister.zsh
+++
@@ -1,7 +0,0 @@
-if [[ `uname -n` == 'lannister' ]]; then
-    WORKON_HOME=~/Envs
-    PROJECT_HOME=~/Projects/Python
-    source /usr/local/bin/virtualenvwrapper.sh
-
-    PATH=$PATH:$HOME/.rvm/bin:./lib/tasks # Add RVM to PATH for scripting
-fi
